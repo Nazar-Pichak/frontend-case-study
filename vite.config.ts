b2ca5d7003b3e72c3adaecf5af8b3297f3c243d1
Nazar Pichak <pichak.nazariy@gmail.com>
2026-08-30 01:30:01 +0200

    refactor: select API base URL by environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+    plugins: [react()],
 
-  server: {
-        proxy: {
-            '/api': {
-                target: 'https://nfctron-frontend-seating-case-study-2024.vercel.app',
-                changeOrigin: true,
-                secure: true,
-                rewrite: (requestPath) =>
-                    requestPath.replace(/^\/api/, ''),
-            },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
         },
     },
-})
+});
